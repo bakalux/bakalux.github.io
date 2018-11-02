fafea2b12ad77ec4e24154a39a232e8db5ec676c
bakalux <swaveful@gmail.com>
2018-11-02 18:25:28 +0300

    implemented removing data

--- main.js
+++ main.js
@@ -4,15 +4,13 @@ let dataArray = [];
 document.getElementById("data").addEventListener("keyup", function(e) {
   e.preventDefault();
   if (e.keyCode === 13) {
-    addData();
+    if (validateInput() === true) {
+      addData();
+    }
     document.getElementById("data").value = "";
   }
 });
-/*
-document.getElementsByClassName("remove").onclick = function(e) {
-  alert(e.target.parentNode.id);
-};
-*/
+
 function addData() {
   const value = document.getElementById("data").value;
   const date = new Date();
@@ -25,15 +23,36 @@ function addData() {
   addToDOM(data);
   globalIdCounter++;
   console.log(dataArray);
+  listenToRemove();
 }
 
 function addToDOM(data) {
   document.getElementById("values").innerHTML += `
-  <li class="value" id=${data.id}>
+  <li class="value">
     <span class="time">${data.date.getMinutes()}:${data.date.getSeconds()}:${data.date.getMilliseconds()}
   </span>
     <span class="number">${data.value}</span>
-    <button class="remove">Remove</button>
+    <button class="remove" id=${data.id}>Remove</button>
   </li>  
   `;
+}
+
+function listenToRemove() {
+  let buttons = document.getElementsByClassName("remove");
+  console.log(buttons);
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+      dataArray = dataArray.filter(function(data) {
+        return parseInt(data.id) !== parseInt(buttons[i].id);
+      });
+      buttons[i].parentNode.remove();
+      console.log(dataArray);
+    });
+  }
+}
+
+function validateInput() {
+  const regExp = /^-?\d+$/;
+  const input = document.getElementById("data").value;
+  return regExp.test(input);
 }
